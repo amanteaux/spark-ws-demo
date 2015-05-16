c2dffa1d6d533aeabca1fc83e90b0a86b118c5a2
Aurélien Manteaux <amanteaux@gmail.com>
2015-05-16 15:49:42 +0200

    Adding angular server status

--- src/main/resources/web/js/app.js
+++ src/main/resources/web/js/app.js
@@ -12,4 +12,32 @@ var app = angular
             templateUrl: 'views/login.html'
         })
 })
+.run(function($rootScope, $resource) {
+            $rootScope.server = {};
+            var serverWs = $resource('/status/');
+            var serverUp = function() {
+                $rootScope.server.statusClass = 'label-success';
+                $rootScope.server.statusLabel = 'Up';
+            };
+            var serverDown = function() {
+                $rootScope.server.statusClass = 'label-danger';
+                $rootScope.server.statusLabel = 'Down';
+            };
+            var updateStatus = function() {
+              serverWs
+                  .get()
+                  .$promise
+                  .then(function(data) {
+                      if(data && data.alive) {
+                          serverUp();
+                      } else {
+                          serverDown();
+                      }
+                  }, serverDown)
+                  .finally(function() {
+                      setTimeout(updateStatus, 1000);
+                  });
+            };
+            updateStatus();
+})
 ;
